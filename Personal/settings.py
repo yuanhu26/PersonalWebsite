d289c75079863a063334e876b08696ee0d517f31
yuanhu26 <yuanhu0326@gmail.com>
2021-09-09 19:49:30 -0400

    micro email

--- Personal/settings.py
+++ Personal/settings.py
@@ -164,11 +164,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ['EMAIL_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASS']
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = os.environ['EMAIL_USER']
+# EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASS']
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST_USER = 'os.environ['EMAIL_USER']'
+EMAIL_HOST_PASSWORD = 'os.environ['EMAIL_PASS']'
+EMAIL_PORT = 25
 
 django_heroku.settings(locals())
