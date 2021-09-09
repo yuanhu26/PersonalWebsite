2342a974e291e28182e073eade7cc0ee107387f6
yuanhu26 <yuanhu0326@gmail.com>
2021-09-09 13:25:34 -0400

    contact

--- Personal/settings.py
+++ Personal/settings.py
@@ -21,10 +21,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(
-    DEBUG = (bool,False)
-    )
-environ.Env.read_env(Path(BASE_DIR, '.env'))
+# env = environ.Env(
+#     DEBUG = (bool,False)
+#     )
+# environ.Env.read_env(Path(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
